0c111ba1cfded3c8534cf249d49ae7a08810978b
Williams <williams@williams-System-Product-Name.(none)>
2013-05-08 14:57:38 -0300

    subirr

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,7 +4,7 @@ class Result < ActiveRecord::Base
   belongs_to :user
   attr_accessible :Value, :Notes
 
-  validates :Value, :numericality => true
+  validates :Value, :numericality => true, :allow_nil => true
   validates :entry_id, :user_id, :question_id, :presence => true
   validates :user_id, :uniqueness => { :scope => [:question_id, :entry_id], :message => "already calificate for the same question and entry" }
 end
